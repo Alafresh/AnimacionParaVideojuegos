e67db6d22ce550145e8bf9f7a0218ac1bcfcdabd
juan_ <alafresh16@gmail.com>
2025-08-05 01:14:42 -0500

    Update CharacterAim.cs

--- Assets/Scripts/BlendTree/CharacterAim.cs
+++ Assets/Scripts/BlendTree/CharacterAim.cs
@@ -16,5 +16,12 @@ public class CharacterAim : MonoBehaviour, ICharacterComponent {
         if(!ctx.started && !ctx.canceled) return;
 
         aimCamera?.gameObject.SetActive(ctx.started);
+        ParentCharacter.IsAiming = ctx.started;
+        aimDamper.TargetValue = ctx.started ? 1f : 0f;
+    }
+
+    private void Update() {
+        aimDamper.Update();
+        anim.SetLayerWeight(1, aimDamper.CurrentValue);
     }
 }
